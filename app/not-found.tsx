0da0f04b87a467e6ac905c83e31a31565394a9e1
dk <kukkudanish@gmail.com>
2025-07-30 10:16:58 +0530

    work on custom error page and write it programitacly

---
+++ app/not-found.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const errorPage = () => {
+  return (
+    <div>
+       Custom error page
+    </div>
+  )
+}
+
+export default errorPage
--- app/products/[productId]/reviews/[reviewId]/page.tsx
+++ app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,6 +1,11 @@
 import React from 'react'
+import { notFound } from 'next/navigation'
 
 const reviewId = ({params} : {params : { productId : string,reviewId : string}}) => {
+  const {reviewId , productId} = params
+  
+  if(parseInt(reviewId) > 1000) notFound()
+
   return (
     <div>
         productId {params.productId} and reviewId {params.reviewId}
